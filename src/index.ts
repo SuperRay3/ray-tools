bbf2f15947a8924ca09cc20cd1705075d9d3dee6
leijialiang <ljl519525771@qq.com>
2021-10-11 13:40:22 +0800

    feat: 增加 fixedRound

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+/**
+ * 保留指定位数小数,并自动补零
+ * @param num 被操作数字
+ * @param m 保留的位数
+ */
+
+ export const fixedRound = (number: number, m = 2): string => {
+  let result = Math.round(Math.pow(10, m) * number) / Math.pow(10, m)
+  let resultStr: string = String(result)
+
+  if (resultStr.indexOf('.') === -1) {
+    if (m !== 0) {
+      resultStr += '.'
+      resultStr += new Array(m + 1).join('0')
+    }
+  } else {
+    const arr = resultStr.split('.')
+    if (arr[1].length < m) {
+      arr[1] += new Array(m - arr[1].length + 1).join('0')
+    }
+    resultStr = arr.join('.')
+  }
+
+  return resultStr
+}
